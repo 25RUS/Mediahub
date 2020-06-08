4c96a56a5fc4d01c01c4b928ce9ab2fe8658afd4
25rus <adnet_9@live.com>
2020-06-08 10:18:28 +1000

    Добавьте файлы проекта.

---
+++ mediahub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mediahub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
